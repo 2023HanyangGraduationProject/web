d1f4bdedc37d6cfaa3c6b93411e504ec2411baca
songjaeheon <spes2you@gmail.com>
2023-09-23 21:31:29 +0900

    feat: 지갑 주소 사용 예시(react hydrate)

--- src/app/listing/page.js
+++ src/app/listing/page.js
@@ -1,4 +1,22 @@
+'use client'
+import React from "react";
+import { useAccount } from 'wagmi'
 
 export default function Page() {
-    return <h1>Hello, Listing Page!</h1>
+    const [hydrated, setHydrated] = React.useState(false);
+    const { address, connector, isConnected } = useAccount()
+    React.useEffect(() => {
+        setHydrated(true);
+    }, []);
+    if (!hydrated) {
+        // Returns null on first render, so the client and server match
+        return null;
+    }
+    return (
+        <div>
+            <h1>Hello, Listing Page!</h1>
+            <div>{address}</div>
+        </div>
+    )
+    
 }
