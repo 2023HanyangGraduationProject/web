100b01e53207843c0b1f8f14b7d39d9b2867446b
songjaeheon <spes2you@gmail.com>
2023-10-30 14:36:05 +0900

    feat: item 페이지 프로토타입

--- public/scripts/collections_id.js
+++ public/scripts/collections_id.js
@@ -1,7 +1,7 @@
 const items = document.getElementById('items')
 const collectionId = window.location.pathname.split('/')[2]
 
-// fetch collections from database by rest api and make a list
+// fetch items belong to a collection from database by rest api and make a list
 // TODO localhost 교체
 fetch('http://localhost:3000/api/collections/'+collectionId, {method: 'GET'})
   .then(res => res.json())
@@ -9,8 +9,8 @@ fetch('http://localhost:3000/api/collections/'+collectionId, {method: 'GET'})
     const list = res.map((ele) => (
       `<li key="${ele.id}">
         <img src="${ele.img}" width="200" height="200" />
+        <a href="/items/${ele.id}">상세 페이지</a>
       </li>`
-        // <a href="/collections/${ele.id}">${ele.id}</a>
     )).join('')
     items.innerHTML = list
   })
