eb90279ea19f9972ba60def3816e0f265b074e12
songjaeheon <spes2you@gmail.com>
2023-11-08 19:23:59 +0900

    feat: config 업데이트(next.js, hardhat - sepolia)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+
+require ("dotenv").config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: API_URL,
+      accounts: [`${PRIVATE_KEY}`]
+    }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  // mocha: {
+  //   timeout: 40000
+  // }
+}
